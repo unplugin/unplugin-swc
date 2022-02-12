d5d10ea31f2a612c2f51432a4ac831cfafd04752
rrosas1198 <rrosas1198@gmail.com>
2022-02-12 09:24:50 -0500

    feat: load tsconfig package implemented

--- src/tsconfig.ts
+++ src/tsconfig.ts
@@ -1,31 +1,16 @@
-import fs from 'fs'
 import path from 'path'
-import { parse } from 'jsonc-parser'
 import JoyCon from 'joycon'
+import { loadTsConfig } from 'load-tsconfig'
 
 const joycon = new JoyCon()
 
 joycon.addLoader({
   test: /\.json$/,
   load(filepath: string) {
-    return readTsconfig(filepath)
+    const loaded = loadTsConfig(path.dirname(filepath))
+    return loaded?.data;
   },
 })
-
-const readTsconfig = async (filepath: string) => {
-  const content = await fs.promises.readFile(filepath, 'utf8')
-  const parsed = parse(content) || {}
-
-  parsed.compilerOptions = parsed.compilerOptions || {}
-
-  if (typeof parsed.extends === 'string') {
-    const rpath = path.resolve(path.dirname(filepath), parsed.extends)
-    const parent = await readTsconfig(rpath)
-    parsed.compilerOptions = Object.assign(parent.compilerOptions, parsed.compilerOptions)
-  }
-
-  return parsed;
-}
 
 export const getCompilerOptions = async (
   file: string,
