1e9b4814bed64f0b4b476558183791177bf46d8a
hanlee <me@hanlee.co>
2023-08-28 11:49:52 +0800

    feat: upgrade to tsup v7

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  format: ['cjs', 'esm'],
+  entry: ['src/index.ts'],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  dts: {
+    resolve: true,
+  },
+})
