09abb69159a02f18848cb57b431eaa91822ee749
Ryan Suhartanto <suhartanto@kekkon.nexus>
2025-10-21 14:39:02 +0700

    style: fix lint errors
    `pnpm exec eslint --fix`

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,2 @@
-import { sxzz } from "@sxzz/eslint-config";
-export default sxzz();
+import { sxzz } from '@sxzz/eslint-config'
+export default sxzz()
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,43 @@
-import path from "node:path";
-import { createFilter, type FilterPattern } from "@rollup/pluginutils";
+import path from 'node:path'
+import { createFilter, type FilterPattern } from '@rollup/pluginutils'
 import {
   transform,
   type JscConfig,
   type Options as SwcOptions,
   type TransformConfig,
-} from "@swc/core";
-import { defu } from "defu";
+} from '@swc/core'
+import { defu } from 'defu'
 // @ts-expect-error missing types
-import { loadTsConfig } from "load-tsconfig";
-import { createUnplugin } from "unplugin";
-import { resolveId } from "./resolve";
+import { loadTsConfig } from 'load-tsconfig'
+import { createUnplugin } from 'unplugin'
+import { resolveId } from './resolve'
 
 export type Options = SwcOptions & {
-  include?: FilterPattern;
-  exclude?: FilterPattern;
-  tsconfigFile?: string | boolean;
-};
+  include?: FilterPattern
+  exclude?: FilterPattern
+  tsconfigFile?: string | boolean
+}
 
 type WithRequiredProperty<Type, Key extends keyof Type> = Type & {
-  [Property in Key]-?: Type[Property];
-};
+  [Property in Key]-?: Type[Property]
+}
 
-type SWCOptions = WithRequiredProperty<JscConfig, "parser" | "transform">;
+type SWCOptions = WithRequiredProperty<JscConfig, 'parser' | 'transform'>
 
 export default createUnplugin<Options | undefined, false>(
   ({ tsconfigFile, minify, include, exclude, ...options } = {}) => {
     const filter = createFilter(
       include || /\.m?[jt]sx?$/,
       exclude || /node_modules/,
-    );
+    )
 
     return {
-      name: "swc",
+      name: 'swc',
 
       resolveId,
 
       async transform(code, id) {
-        if (!filter(id)) return null;
+        if (!filter(id)) return null
 
         const compilerOptions =
           tsconfigFile === false
@@ -45,22 +45,22 @@ export default createUnplugin<Options | undefined, false>(
             : loadTsConfig(
                 path.dirname(id),
                 tsconfigFile === true ? undefined : tsconfigFile,
-              )?.data?.compilerOptions || {};
+              )?.data?.compilerOptions || {}
 
-        const isTs = /\.m?tsx?$/.test(id);
+        const isTs = /\.m?tsx?$/.test(id)
 
         let jsc: SWCOptions = {
           parser: {
-            syntax: isTs ? "typescript" : "ecmascript",
+            syntax: isTs ? 'typescript' : 'ecmascript',
           },
           transform: {},
-        };
+        }
 
         if (compilerOptions.jsx) {
-          if (jsc.parser.syntax === "typescript") {
-            jsc.parser.tsx = true;
+          if (jsc.parser.syntax === 'typescript') {
+            jsc.parser.tsx = true
           } else {
-            jsc.parser.jsx = true;
+            jsc.parser.jsx = true
           }
           Object.assign<TransformConfig, TransformConfig>(jsc.transform, {
             react: {
@@ -68,30 +68,30 @@ export default createUnplugin<Options | undefined, false>(
               pragmaFrag: compilerOptions.jsxFragmentFactory,
               importSource: compilerOptions.jsxImportSource,
             },
-          });
+          })
         }
 
         if (compilerOptions.experimentalDecorators) {
           // class name is required by type-graphql to generate correct graphql type
-          jsc.keepClassNames = true;
-          jsc.parser.decorators = true;
+          jsc.keepClassNames = true
+          jsc.parser.decorators = true
           Object.assign<TransformConfig, TransformConfig>(jsc.transform!, {
             legacyDecorator: true,
             decoratorMetadata: compilerOptions.emitDecoratorMetadata,
-          });
+          })
         }
 
         if (compilerOptions.target) {
-          jsc.target = compilerOptions.target;
+          jsc.target = compilerOptions.target
         }
 
         if (compilerOptions.useDefineForClassFields != null) {
           jsc.transform.useDefineForClassFields =
-            compilerOptions.useDefineForClassFields;
+            compilerOptions.useDefineForClassFields
         }
 
         if (options.jsc) {
-          jsc = defu<SWCOptions, SWCOptions[]>(options.jsc, jsc);
+          jsc = defu<SWCOptions, SWCOptions[]>(options.jsc, jsc)
         }
 
         const result = await transform(code, {
@@ -99,18 +99,18 @@ export default createUnplugin<Options | undefined, false>(
           sourceMaps: true,
           ...options,
           jsc,
-        });
+        })
         return {
           code: result.code,
           map: result.map && JSON.parse(result.map),
-        };
+        }
       },
 
       vite: {
         config() {
           return {
             esbuild: false,
-          };
+          }
         },
       },
 
@@ -121,15 +121,15 @@ export default createUnplugin<Options | undefined, false>(
               sourceMaps: true,
               minify: true,
               filename: chunk.fileName,
-            });
+            })
             return {
               code: result.code,
               map: result.map,
-            };
+            }
           }
-          return null;
+          return null
         },
       },
-    };
+    }
   },
-);
+)
