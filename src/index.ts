509d6d93a4a84012ed1cea7b781233f959be1990
Han <me@hanlee.co>
2026-08-04 00:43:23 +0900

    fix: correct Vite oxc config syntax

--- src/index.ts
+++ src/index.ts
@@ -108,7 +108,7 @@ export default createUnplugin<Options | undefined, false>(
         config() {
           return {
             esbuild: false,
-            oxc, false,
+            oxc: false,
           }
         },
       },
